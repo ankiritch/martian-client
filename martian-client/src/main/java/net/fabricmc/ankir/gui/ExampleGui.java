b0818b63bc2a8e33cd0c7480ce31fd92a197aff4
ankiritch <47478331+ankiritch@users.noreply.github.com>
2021-01-24 17:48:29 +0100

    Gui Update
    Done

--- martian-client/src/main/java/net/fabricmc/ankir/gui/ExampleGui.java
+++ martian-client/src/main/java/net/fabricmc/ankir/gui/ExampleGui.java
@@ -2,11 +2,16 @@ package net.fabricmc.ankir.gui;
 
 import io.github.cottonmc.cotton.gui.client.LightweightGuiDescription;
 import io.github.cottonmc.cotton.gui.widget.WGridPanel;
+import io.github.cottonmc.cotton.gui.widget.WLabel;
 
 public class ExampleGui extends LightweightGuiDescription {
     public ExampleGui() {
         WGridPanel root = new WGridPanel();
         setRootPanel(root);
-        root.setSize(50, 20);
+        root.setSize(100, 20);
+
+
+        WLabel label = new WLabel("Hello Team Mars!");
+        root.add(label, 1, 1);
     }
 }
