ef2bc7e01e7bf3ee6fc63355460f7d4a83357efe
ankiritch <47478331+ankiritch@users.noreply.github.com>
2021-01-24 17:16:12 +0100

    New Test
    Working

---
+++ martian-client/src/main/java/net/fabricmc/ankir/Main.java
@@ -0,0 +1,10 @@
+package net.fabricmc.ankir;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Main implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		System.out.println("Hello Fabric world!");
+	}
+}
